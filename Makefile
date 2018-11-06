0a08f0945ddc3285ba6fd33df1d84f541e88da4d
geoliak <g.liakopoulos@hotmail.com>
2018-11-06 13:21:53 +0100

    lect prioritaires

--- Makefile
+++ Makefile
@@ -6,8 +6,11 @@ all:	test_lecteurs_redacteurs
 
 test_lecteurs_redacteurs:	test_lecteurs_redacteurs.o lecteur_redacteur.o
 
-lecteur_redacteur.o:
-	$(CC) $(CFLAGS) -c lecteur_redacteur.c 
+lecteur_redacteur.o:	lecteur_redacteur.c
+	$(CC) $(CFLAGS) -c lecteur_redacteur.c
+
+test_lecteurs_redacteurs.o:
+	$(CC) $(CFLAGS) -c test_lecteurs_redacteurs.c
 
 clean:
-	rm test_lecteurs_redacteurs *.o 
+	rm test_lecteurs_redacteurs *.o
--- lecteur_redacteur.c
+++ lecteur_redacteur.c
@@ -1,11 +1,15 @@
 #include "lecteur_redacteur.h"
+int lecteur_en_attente = 0 ;
+
 
 void debut_lecture(lecteur_redacteur_t *lec_red){
 	//si nb_lecteurs = 0 alors wait sur le sémaphore puis nb_lecteurs++, sinon seulement nb_lecteurs++
 	pthread_mutex_lock(&lec_red->verrou_global);
+	lecteur_en_attente = 1;
 	if (lec_red->nb_lecteurs==0) {
 		sem_wait(&lec_red->sem);
 	}
+	lecteur_en_attente = 0;
 	lec_red->nb_lecteurs++;
 	pthread_mutex_unlock(&lec_red->verrou_global);
 }
@@ -23,6 +27,11 @@ void fin_lecture(lecteur_redacteur_t *lec_red){
 void debut_redaction(lecteur_redacteur_t *lec_red){
 	//wait sur le sémaphore dans tout les cas
 	sem_wait(&lec_red->sem);
+	while (lecteur_en_attente != 0) {
+		sem_post(&lec_red->sem);
+		sem_wait(&lec_red->sem);
+	}
+
 }
 
 void fin_redaction(lecteur_redacteur_t *lec_red){
